a98dcdc50fdbcc8b3766101f09fedc51c72d8e08
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2021-04-07 22:27:12 -0300

    Create temporary backend application

---
+++ packages/api/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const port = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Lerna!');
+});
+
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}`);
+});
