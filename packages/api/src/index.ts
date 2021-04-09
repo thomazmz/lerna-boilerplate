225fe3c2e66190972cc2a12f3bedd7553a3e1208
Thomaz Zandonotto <thomaz.zandonotto@gmail.com>
2021-04-08 22:17:21 -0300

    Create api .gitignore file

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,16 +1,12 @@
-// import express from 'express';
+import express from 'express';
 
-// const port = 3000;
-// const app = express();
+const port = 3000;
+const app = express();
 
-// app.get('/', (req, res) => {
-//   res.send('Hello Lerna!');
-// });
+app.get('/', (req, res) => {
+  res.send('Hello Lerna!');
+});
 
-console.log('Hello')
-console.log(process.env.A);
-console.log(process.env.B);
-
-// app.listen(port, () => {
-//   console.log(`Application is running on port ${port}`);
-// });
+app.listen(port, () => {
+  console.log(`Application is running on port ${port}`);
+});
